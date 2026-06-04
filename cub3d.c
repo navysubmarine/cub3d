69121efdc3e71b99ced4c65472bd7be757fc1f20
marthoma <marthoma@student.42.fr>
2026-06-04 12:47:21 +0200

    ok is_there_one_player

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: marthoma <marthoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 11:46:19 by marthoma          #+#    #+#             */
-/*   Updated: 2026/06/04 12:32:32 by marthoma         ###   ########.fr       */
+/*   Updated: 2026/06/04 12:46:08 by marthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,10 +141,46 @@ int	are_walls_enclosed(t_game *g, char **map)
 	
 }
 
-/*what is the point to passing g ? and not the map ?*/
-int	is_there_one_player(char *line)
+int	where_is_the_player(t_game *g, char **map)
 {
 	
+}
+
+int	is_there_one_player(char **map)
+{
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	j = 0;
+	count = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'N' || map[i][j] == 'S'
+				|| map[i][j] == 'W' || map[i][j] == 'E')
+				count ++;
+			j++;
+		}
+		i++;
+	}
+	if (count != 1)
+		return (1);
+	return (0);
+}
+
+int	handle_player_pos(t_game *g, char **map)
+{
+	if (is_there_one_player(map))
+	{
+		printf("Error. There is not exactly one player\n");
+		return (1);
+	}
+	where_is_the_player(g, map);
+	return (0);	
 }
 
 int	is_map_playable(t_game *g)
@@ -154,8 +190,14 @@ int	is_map_playable(t_game *g)
 
 	i = 0;
 	copy = map_copy(g->map.map);
+	if (!copy)
+	{
+		ft_putstr_fd("Error. Map couldn't be copied\n", 2);
+		return (1);
+	}
 	/*is there only one player and where is it*/
-	handle_player_pos();
+	if (handle_player_pos(g, copy))
+		return (1);
 	if (are_walls_enclosed(g, copy))
 	{
 		printf("Error. Map walls are not closed\n", i);
